069c818287861f13dcd67718744f3bbed4a5c64f
yolles <yolles@adobe.com>
2015-08-07 23:19:20 -0700

    Add comments to blog posts and blog list
    Add comments to blog posts. Add comments to blog list. Add comments
    to blog list view. Update index and JS to apply sendmail to just the
    sendmail form. Add JS to move comment/reply form. Add CSS to hide
    "Add comment" link.

--- build/js/custom.js
+++ build/js/custom.js
@@ -83,7 +83,7 @@ $(function(){
     $status = $result.find('.status'),
     $message = $result.find('.message');
 
-  $('input,textarea').jqBootstrapValidation({
+  $('.email-input').jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
@@ -161,3 +161,50 @@ $(window).resize(close_toggle);
 
   wow.init();
 })();
+
+/**
+ * Handle comment submission. Currently the form is a full postback but will
+ * become an AJAX call.
+ */
+$(function() {
+  var $commentForm = $('#commentForm'),
+      $newCommentContainer = $('#newCommentContainer'),
+      $addCommentContainer = $('#addCommentContainer'),
+      $commentListContainer = $('#commentListContainer'),
+      $commentPathField = $('#commentPath'),
+      $formHeader = $('#formHeader'),
+      $formButton = $('#formButton');
+
+  /**
+   * Move the form back to its orginal location and reset the path field to
+   * blank as it's no longer a reply post but a top level comment. Hide the add
+   * comment link as the form is in the correct place.
+   */
+  $addCommentContainer.click(function(e) {
+    e.preventDefault();
+    $newCommentContainer.append($commentForm);
+    $addCommentContainer.hide();
+    $commentPathField.val('');
+    $formHeader.text($formHeader.data('comment-text'));
+    $formButton.text($formButton.data('comment-text'));
+  });
+
+  /**
+   * Move the form under under the comment that the user wants to create a reply
+   * for. Show the Add Comment link at the bottom of the list. Populate the form
+   * field for comment path with the parent comment as that is the parent node
+   * to create the reply to.
+   */
+  $commentListContainer.on('click', '.comment-reply', function(e) {
+    var $this = $(this),
+        $listItem = $this.parents('.comment-item-container'),
+        commentPath = $listItem.data('comment-path');
+
+    e.preventDefault();
+    $addCommentContainer.show();
+    $listItem.append($commentForm);
+    $commentPathField.val(commentPath);
+    $formHeader.text($formHeader.data('reply-text'));
+    $formButton.text($formButton.data('reply-text'));
+  });
+});
